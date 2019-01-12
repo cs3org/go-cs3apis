2cdb78083ef0d69fb5ab476ecf23cab3581790f1
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2019-01-12 12:23:34 +0100

    build: use build.sh

--- Makefile
+++ Makefile
@@ -1,10 +1,4 @@
 all:
-	git clone https://github.com/cernbox/cs3apis build && cd build && make
-	cat prototool_gen_go.yaml >> build/prototool.yaml
-	cd build && prototool generate
-	rm -rf build
-	# change import paths
-	egrep -lR 'github.com/cernbox/go-cs3apis/cs3/' | xargs sed -i -e 's|github.com/cernbox/go-cs3apis/cs3/|github.com/cernbox/go-cs3apis/cs3/|g'
-
+	./build.sh
 clean:
 	rm -rf build
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GOPATH=`go env GOPATH`
+PATH=$PATH:$GOPATH/bin
+git clone https://github.com/cernbox/cs3apis build && cd build && make && cd ..
+cat prototool_gen_go.yaml >> build/prototool.yaml
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+go get -u github.com/golang/protobuf/protoc-gen-go
+cd build && prototool generate && cd ..
+rm -rf build
+
+# change import paths
+egrep -lR 'github.com/cernbox/go-cs3apis/cs3/' . | xargs sed -i '' 's|github.com/cernbox/go-cs3apis/cs3/|github.com/cernbox/go-cs3apis/cs3/|g'
